49390e4487e5b0edec6c92e595e459aecdbb0410
Gpanazio <panazio@gmail.com>
2025-07-25 15:54:22 -0300

    Add files via upload

--- src/AdminEquipamentos.jsx
+++ src/AdminEquipamentos.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { supabase } from './supabase'
+import { supabase } from './lib/supabase'
 
 function AdminEquipamentos() {
   const [equipamentos, setEquipamentos] = useState([])
